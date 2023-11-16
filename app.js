0ad378c0c131aae992d10bfdc0ce4557f887bf13
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-11-16 10:59:42 -0600

    se instala y confihura swagger

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require('cors');
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./doc/swagger");
 
 
 const app = express();
+app.use(cors());
 
-app.use(logger('dev'));
+// Configura Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api', require('./routes/logs.routes'));
-app.use('/api', require('./routes/application.routes'));
-app.use('/api', require('./routes/authorization.routes'));
-
+app.use("/api", require("./routes/logs.routes"));
+app.use("/api", require("./routes/application.routes"));
+app.use("/api", require("./routes/authorization.routes"));
 
 module.exports = app;
---
+++ doc/swagger.js
@@ -0,0 +1,17 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0', // Especifica la versión de OpenAPI (Swagger)
+    info: {
+      title: 'API backend',
+      version: '1.0.0',
+      description: 'Documentación de la API Express utilizando Swagger',
+    },
+  },
+  apis: ['./routes/*.js'], // Rutas a tus archivos de definición de rutas
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
