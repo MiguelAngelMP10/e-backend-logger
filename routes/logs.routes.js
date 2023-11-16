03edea996a0e0ed709025df0e419336501761b1f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-11-15 22:16:48 -0600

    add auth

--- routes/logs.routes.js
+++ routes/logs.routes.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 
-const router = require('express').Router();
-const prefix = '/logs';
+const router = require("express").Router();
+const auth = require("../middleware/auth");
+const prefix = "/logs";
 
-const controller = require('../controllers/logs.controller');
+const controller = require("../controllers/logs.controller");
 
-router.get(`${prefix}/`, controller.all);
-router.post(`${prefix}/`, controller.create);
-router.get(`${prefix}/:id`, controller.info);
-router.put(`${prefix}/:id`, controller.update);
-router.delete(`${prefix}/:id`, controller.delete);
+router.get(`${prefix}/`, auth, controller.all);
+router.post(`${prefix}/`, auth, controller.create);
+router.get(`${prefix}/:id`, auth, controller.info);
+router.put(`${prefix}/:id`, auth, controller.update);
+router.delete(`${prefix}/:id`, auth, controller.delete);
 
 module.exports = router;
